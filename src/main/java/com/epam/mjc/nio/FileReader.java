59705af754d869b03286c62ae5be0a1e859a9910
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-11-26 09:43:25 +0300

    feat: complete task1 module7 stage1 v.0

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,59 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        InputStream inputStream = getFileFromResourceAsStream(String.valueOf(file));
+        Profile profile = null;
+        try ( InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+              BufferedReader reader = new BufferedReader(streamReader)) {
+            String line;
+            line = reader.readLine();
+            profile = new Profile();
+            while (line != null) {
+                String key = line.split(":")[0];
+                String str;
+                switch (key) {
+                    case "Name":
+                        str = line.split(":")[1];
+                        profile.setName(str.trim());
+                        break;
+                    case "Age":
+                        str = line.split(":")[1];
+                        Integer age = Integer.valueOf(str.trim());
+                        profile.setAge(age);
+                        break;
+                    case "Email":
+                        str = line.split(":")[1];
+                        profile.setEmail(str.trim());
+                        break;
+                    case "Phone":
+                        str = line.split(":")[1];
+                        profile.setPhone(Long.valueOf(str.trim()));
+                        break;
+                    default:
+                        break;
+                }
+                line = reader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
+    }
+
+    private InputStream getFileFromResourceAsStream(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
     }
 }
